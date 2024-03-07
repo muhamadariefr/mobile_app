3b0933ce39a98b9e46ad0c9110682e7eeb9d83ee
muhamadariefr <m.arief.r54321@gmail.com>
2024-03-07 08:56:07 +0700

    tugas 1 flutter

---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueAccent,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Header
+              AppBar(
+                title: Text(
+                  'Biodata',
+                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold, color: Colors.white),
+                ),
+                centerTitle: true,
+                backgroundColor: Colors.blueAccent,
+              ),
+              // Content
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage('assets/Pas Foto.jpg'),
+              ),
+              Text(
+                "M Arief Rahmatulloh",
+                style: TextStyle(
+                    fontSize: 28.0,
+                    color: Colors.white,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Mahasiswa",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+              SizedBox(
+                height: 19.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              // Cards
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.account_circle_rounded,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(width: 20.0),
+                      Text(
+                        'Muhamad Arief Rahmatulloh',
+                        style: TextStyle(fontSize: 18.0, color: Colors.lightBlue),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.card_membership,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(width: 20.0),
+                      Text(
+                        '2041013',
+                        style: TextStyle(fontSize: 18.0, color: Colors.lightBlue),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.computer,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(width: 20.0),
+                      Text(
+                        'S1 Teknik Informatika',
+                        style: TextStyle(fontSize: 18.0, color: Colors.lightBlue),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 20.0),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.school,
+                        color: Colors.blue,
+                      ),
+                      SizedBox(width: 20.0),
+                      Text(
+                        'STT Mandala Bandung',
+                        style: TextStyle(fontSize: 18.0, color: Colors.lightBlue),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              // Footer
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Text(
+                      '© 2024 muhamadariefr',
+                      style: TextStyle(
+                        fontSize: 14.0,
+                        color: Colors.white,                        
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
